f43d33d625f2a4851a4288e23fbdecc762385d27
tylercare <tyler.vickers@dignityhealth.org>
2018-03-19 15:55:36 -0700

    series of Unions to get fieldnames and distinct values for each field-ALL

---
+++ ADT systems/Fields_and_distinct_values.sql
@@ -0,0 +1,135 @@
+SELECT distinct 'firstname' as 'field_name', firstname as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'lastname' as 'field_name', lastname as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'gender' as 'field_name', gender as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'dateofbirth' as 'field_name', dateofbirth as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'adtsystemname' as 'field_name', adtsystemname as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'medicalrecordnumber' as 'field_name', medicalrecordnumber as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'accountnumber' as 'field_name', accountnumber as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'facilitycode' as 'field_name', facilitycode as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'facilityname' as 'field_name', facilityname as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'admissiondate' as 'field_name', admissiondate as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'dischargedate' as 'field_name', dischargedate as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'financialclasscode' as 'field_name', financialclasscode  as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'financialclassdescription' as 'field_name', financialclassdescription  as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'patienttype' as 'field_name', patienttype as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'patienttypedesc' as 'field_name', patienttypedesc as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'servicelinecode' as 'field_name', servicelinecode as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'servicelinedesc' as 'field_name', servicelinedesc as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'serviceareacode' as 'field_name', serviceareacode as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'serviceareadesc' as 'field_name', serviceareadesc as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'middleinitial' as 'field_name', middleinitial as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'city' as 'field_name', city as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'state' as 'field_name', state as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'zip' as 'field_name', zip as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'email' as 'field_name', email as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'phone' as 'field_name', phone as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'primaryinsurancename' as 'field_name', primaryinsurancename as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'emailmarketingoptin' as 'field_name', emailmarketingoptin as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'region' as 'field_name', region as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'facility' as 'field_name', facility as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'addressline1' as 'field_name', addressline1 as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'addressline2' as 'field_name', addressline2 as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'patientmaritalstatus' as 'field_name', patientmaritalstatus as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'admissionfinaldate' as 'field_name', admissionfinaldate as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'primaryfinancialclass' as 'field_name', primaryfinancialclass as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'admissionyear' as 'field_name', admissionyear as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'admissionmonth' as 'field_name', admissionmonth as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'servicecode' as 'field_name', servicecode as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'agefromadmission' as 'field_name', agefromadmission as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'surgicalprocedure1' as 'field_name', surgicalprocedure1 as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'surgicalprocedure2' as 'field_name', surgicalprocedure2 as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'surgicalprocedure3' as 'field_name', surgicalprocedure3 as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'drgcategory' as 'field_name', drgcategory as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'birthyear' as 'field_name', birthyear as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'firstproceduredate' as 'field_name', firstproceduredate as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'lastservicedate' as 'field_name', lastservicedate as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'doctornumber' as 'field_name', doctornumber as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'hospitalservicecode' as 'field_name', hospitalservicecode as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'timewaited' as 'field_name', timewaited as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'facilityrating' as 'field_name', facilityrating as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'inquickerrating' as 'field_name', inquickerrating as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'devicecategory' as 'field_name', devicecategory as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'secureid' as 'field_name', secureid as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'location' as 'field_name', `location` as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'registrationdate' as 'field_name', registrationdate as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'treatmentdate' as 'field_name', treatmentdate as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'referralcode' as 'field_name', referralcode as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'pregnant' as 'field_name', pregnant as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'weekspregnant' as 'field_name', weekspregnant as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'caregiver' as 'field_name', caregiver as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'newtofacility' as 'field_name', newtofacility as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'medium' as 'field_name', medium as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'source' as 'field_name', source as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'campaign' as 'field_name', campaign as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'referringurl' as 'field_name', referringurl as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'primarycarephysician' as 'field_name', primarycarephysician as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'remoteuid' as 'field_name', remoteuid as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'clientid' as 'field_name', clientid as 'field_value' FROM application_mls.mls_neustar
+UNION
+SELECT distinct 'dhomeid' as 'field_name', dhomeid as 'field_value' FROM application_mls.mls_neustar
